e4527371df8fd6f3dd92c0acf4bd81e1b51dcef6
berdos <b.erdos6@gmail.com>
2022-02-25 19:00:03 +0100

    Update install.R

--- bioc_scripts/install.R
+++ bioc_scripts/install.R
@@ -1,4 +1,6 @@
 install.packages("BiocManager", repos="https://cran.rstudio.com")
+install.packages(c("tidyverse", "ggpubr", "vegan",  "DHARMa", "furrr", "VGAM", "cds", "gridExtra", "pracma", "DoE.base", "AlgDesign", "factoextra", "readxl", "reshape2","data.table", "dtw", "optparse", "pdist", "proxy"))
+
 
 BiocManager::install(version="3.15", update=TRUE, ask=FALSE)
 
